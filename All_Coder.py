9c109f959cf27bfa74ea05386d968cfbfbf450e4
vishal-B-52 <75923911+vishal-B-52@users.noreply.github.com>
2020-12-14 07:21:55 +0530

    Add files via upload

---
+++ All_Coder.py
@@ -0,0 +1,18 @@
+# Nested-if :
+
+n = int(input("Enter a number: "))
+if n > 10:
+    print("Number is greate than 10.")
+    if n % 2==0:
+        print("Number is even")
+    else:
+        print("Number is Odd")
+else:
+    print("Number is smaller than or equal to 10")
+    
+
+
+
+
+    
+    
